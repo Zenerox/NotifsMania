72b4b2de8f5429bcdd251416d408afa240f7515b
Zenerox <31919012+Zenerox@users.noreply.github.com>
2021-02-10 10:14:03 +0100

    Ajout assets + modif base, nav et flash.html.twig

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../public/css/app.css';
 
 // start the Stimulus application
 import './bootstrap';
